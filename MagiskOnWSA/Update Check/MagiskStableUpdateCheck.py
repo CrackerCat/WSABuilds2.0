35bd8cd2b9a98da46f41a398101ab2fc57d02676
MustardChef <68516357+MustardChef@users.noreply.github.com>
2023-11-13 19:02:55 +0000

    Update MagiskStableUpdateCheck.py

--- MagiskOnWSA/Update Check/MagiskStableUpdateCheck.py
+++ MagiskOnWSA/Update Check/MagiskStableUpdateCheck.py
@@ -3,6 +3,7 @@ import json
 import requests
 import logging
 import subprocess
+
 logging.captureWarnings(True)
 env_file = os.getenv('GITHUB_ENV')
 new_version_found = False
@@ -10,8 +11,14 @@ git = (
     "git checkout -f update || git switch --discard-changes --orphan update"
 )
 currentver = requests.get(f"https://raw.githubusercontent.com/WellCodeIsDelicious/WSATest/update/magiskstable.appversion").text.replace('\n', '')
-with open('magiskstable.appversion', 'w') as file:
+
+# Define the path to the file in the home directory
+home_dir = os.path.expanduser('~')
+file_path = os.path.join(home_dir, 'magiskcanary.appversion')
+
+with open(file_path, 'w') as file:
     file.write(currentver)
+
 if not new_version_found:
     # Get latest version
     latestver = ""
@@ -22,10 +29,10 @@ if not new_version_found:
         print("New version found: " + latestver)
         new_version_found = True
         subprocess.Popen(git, shell=True, stdout=None, stderr=None, executable='/bin/bash').wait()
-        with open('magiskstable.appversion', 'w+') as file:
+        with open(file_path, 'w+') as file:
             file.seek(0)
             file.truncate()
             file.write(latestver)
         with open(env_file, "a") as wr:
             wr.write(f"MAGISK_STABLE_MSG={magiskstablemsg}\n")
-    file.close()        
+    file.close()
