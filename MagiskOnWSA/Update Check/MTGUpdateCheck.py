987e0f9f62e23adb75de5c3c5524c6e06d77d924
MustardChef <68516357+MustardChef@users.noreply.github.com>
2023-11-14 12:49:06 +0000

    Update MTGUpdateCheck.py

--- MagiskOnWSA/Update Check/MTGUpdateCheck.py
+++ MagiskOnWSA/Update Check/MTGUpdateCheck.py
@@ -13,18 +13,23 @@ git = (
 with open('gapps.appversion', 'w') as file:
   file.write(currentver)
 if not new_version_found:
-  latestver = ""
-  mtgmsg = ""
-  latestver = json.loads(requests.get(f"https://api.github.com/repos/YT-Advanced/MindTheGappsBuilder/releases/latest").content)['name'].replace('\n', '')
-  mtgmsg="Update MindTheGapps Version from `v" + currentver + "` to `v" + latestver + "`"
-  if currentver != latestver:
-    print("New version found: " + latestver)
-    new_version_found = True
-    subprocess.Popen(git, shell=True, stdout=None, stderr=None, executable='/bin/bash').wait()
-    with open('gapps.appversion', 'w+') as file:
-      file.seek(0)
-      file.truncate()
-      file.write(latestver)
-    with open(env_file, "a") as wr:
-       wr.write(f"MTG_MSG={mtgmsg}\n")
+    latestver = ""
+    mtgmsg = ""
+    latestver = json.loads(requests.get(f"https://api.github.com/repos/YT-Advanced/MindTheGappsBuilder/releases/latest").content)['name'].replace('\n', '')
+    mtgmsg="Update MindTheGapps Version from `v" + currentver + "` to `v" + latestver + "`"
+    if currentver != latestver:
+        print("New version found: " + latestver)
+        new_version_found = True
+        subprocess.Popen(git, shell=True, stdout=None, stderr=None, executable='/bin/bash').wait()
+        with open('gapps.appversion', 'w+') as file:
+            file.seek(0)
+            file.truncate()
+            file.write(latestver)
+        with open(env_file, "a") as wr:
+           wr.write(f"MTG_MSG={mtgmsg}\n")
+    else:
+        mtgmsg = "MindTheGapps Package Version: `" + latestver + "`"
+        with open(env_file, "a") as wr:
+           wr.write(f"MTG_MSG={mtgmsg}\n")
+     
   file.close()   
