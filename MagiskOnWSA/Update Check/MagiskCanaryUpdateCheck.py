b53d678e98b68fae2a7d679ac9a6280b3579c5ce
MustardChef <68516357+MustardChef@users.noreply.github.com>
2023-11-14 11:10:19 +0000

    Update MagiskCanaryUpdateCheck.py

--- MagiskOnWSA/Update Check/MagiskCanaryUpdateCheck.py
+++ MagiskOnWSA/Update Check/MagiskCanaryUpdateCheck.py
@@ -6,26 +6,21 @@ import subprocess
 logging.captureWarnings(True)
 env_file = os.getenv('GITHUB_ENV')
 new_version_found = False
-git = (
-    "git checkout -f update || git switch --discard-changes --orphan update"
-)
-currentver = requests.get(f"https://raw.githubusercontent.com/WellCodeIsDelicious/WSATest/update/magiskstable.appversion").text.replace('\n', '')
-with open('magiskstable.appversion', 'w') as file:
+currentver = requests.get(f"https://raw.githubusercontent.com/WellCodeIsDelicious/WSATest/update/magiskcanary.appversion").text.replace('\n', '')
+with open('magiskcanary.appversion', 'w') as file:
     file.write(currentver)
 if not new_version_found:
-    # Get latest version
     latestver = ""
-    magiskstablemsg = ""
-    latestver = json.loads(requests.get(f"https://github.com/topjohnwu/magisk-files/raw/master/stable.json").content)['magisk']['version'].replace('\n', '')
-    magiskstablemsg="Update Magisk Stable Version from `v" + currentver + "` to `v" + latestver + "`"
+    magiskcanarymsg = ""
+    latestver = json.loads(requests.get(f"https://github.com/topjohnwu/magisk-files/raw/master/canary.json").content)['magisk']['version'].replace('\n', '')
+    magiskcanarymsg="Update Magisk Canary Version from `v" + currentver + "` to `v" + latestver + "`"
     if currentver != latestver:
         print("New version found: " + latestver)
         new_version_found = True
-        subprocess.Popen(git, shell=True, stdout=None, stderr=None, executable='/bin/bash').wait()
-        with open('magiskstable.appversion', 'w+') as file:
+        with open('magiskcanary.appversion', 'w+') as file:
             file.seek(0)
             file.truncate()
             file.write(latestver)
         with open(env_file, "a") as wr:
-            wr.write(f"MAGISK_STABLE_MSG={magiskstablemsg}\n")
-    file.close()        
+            wr.write(f"MAGISK_CANARY_MSG={magiskcanarymsg}\n")
+     file.close()        
