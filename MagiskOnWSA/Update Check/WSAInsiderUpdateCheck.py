e64343830ace1dc6ee56e538ee07ac1fc78c04ce
MustardChef <68516357+MustardChef@users.noreply.github.com>
2023-11-17 11:51:15 +0000

    Update WSAInsiderUpdateCheck.py

--- MagiskOnWSA/Update Check/WSAInsiderUpdateCheck.py
+++ MagiskOnWSA/Update Check/WSAInsiderUpdateCheck.py
@@ -65,8 +65,12 @@ except:
 
 users = {"", user_code}
 
-# The code inside the function WSAInsiderUpdateChecker starts here
-currentver = requests.get(f"https://raw.githubusercontent.com/MustardChef/WSABuilds2.0/update/WIF.appversion").text.replace('\n', '')
+response = requests.get(f"https://raw.githubusercontent.com/MustardChef/WSABuilds2.0/update/WIF.appversion")
+if response.status_code == 200:
+    currentver = response.text.replace('\n', '')
+else:
+    print(f"Failed to fetch version: HTTP {response.status_code}")
+    currentver = ""
 
 # Write for pushing later
 file = open('WIF.appversion', 'w')
