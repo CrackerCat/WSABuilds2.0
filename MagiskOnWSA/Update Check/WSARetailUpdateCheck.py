638f621b8f3bf71f194b164c0adc62ded5504174
MustardChef <68516357+MustardChef@users.noreply.github.com>
2023-11-18 11:57:18 +0000

    Update WSARetailUpdateCheck.py

--- MagiskOnWSA/Update Check/WSARetailUpdateCheck.py
+++ MagiskOnWSA/Update Check/WSARetailUpdateCheck.py
@@ -55,7 +55,6 @@ currentver = requests.get(f"https://raw.githubusercontent.com/MustardChef/WSABui
 file = open('retail.appversion', 'w')
 file.write(currentver)
 
-
 print("Current working directory:", os.getcwd())
 print("Files in '/home/runner/work/WSABuilds2.0/WSABuilds2.0/MagiskOnWSA/xml':", os.listdir('/home/runner/work/WSABuilds2.0/WSABuilds2.0/MagiskOnWSA/xml'))
 
