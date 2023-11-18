391ceaab6c73751648cfc629c926fe87cfcf0b65
MustardChef <68516357+MustardChef@users.noreply.github.com>
2023-11-18 11:52:19 +0000

    Update WSAInsiderUpdateCheck.py

--- MagiskOnWSA/Update Check/WSAInsiderUpdateCheck.py
+++ MagiskOnWSA/Update Check/WSAInsiderUpdateCheck.py
@@ -77,6 +77,9 @@ currentver = requests.get(f"https://raw.githubusercontent.com/MustardChef/WSABui
 file = open('WIF.appversion', 'w')
 file.write(currentver)
 
+print("Current working directory:", os.getcwd())
+print("Files in '/home/runner/work/WSABuilds2.0/WSABuilds2.0/MagiskOnWSA/xml':", os.listdir('/home/runner/work/WSABuilds2.0/WSABuilds2.0/MagiskOnWSA/xml'))
+
 if not new_version_found:
     # Get information
     with open("/home/runner/work/WSABuilds2.0/WSABuilds2.0/MagiskOnWSA/xml/GetCookie.xml", "r") as f:
